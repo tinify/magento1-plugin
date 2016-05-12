89cf3b276ead256708f7aa877381b15854ce229b
Michiel Gerritsen <michiel.gerritsen@tig.nl>
2016-05-12 14:40:30 +0200

    Renamed the module from TIG_Tinypng to TIG_TinyPNG + added translation files

---
+++ app/code/community/TIG/TinyPNG/Block/Adminhtml/System/Config/Form.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ *                  ___________       __            __
+ *                  \__    ___/____ _/  |_ _____   |  |
+ *                    |    |  /  _ \\   __\\__  \  |  |
+ *                    |    | |  |_| ||  |   / __ \_|  |__
+ *                    |____|  \____/ |__|  (____  /|____/
+ *                                              \/
+ *          ___          __                                   __
+ *         |   |  ____ _/  |_   ____ _______   ____    ____ _/  |_
+ *         |   | /    \\   __\_/ __ \\_  __ \ /    \ _/ __ \\   __\
+ *         |   ||   |  \|  |  \  ___/ |  | \/|   |  \\  ___/ |  |
+ *         |___||___|  /|__|   \_____>|__|   |___|  / \_____>|__|
+ *                  \/                           \/
+ *                  ________
+ *                 /  _____/_______   ____   __ __ ______
+ *                /   \  ___\_  __ \ /  _ \ |  |  \\____ \
+ *                \    \_\  \|  | \/|  |_| ||  |  /|  |_| |
+ *                 \______  /|__|    \____/ |____/ |   __/
+ *                        \/                       |__|
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to servicedesk@totalinternetgroup.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact servicedesk@totalinternetgroup.nl for more information.
+ *
+ * @copyright   Copyright (c) 2016 Total Internet Group B.V. (http://www.totalinternetgroup.nl)
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+class TIG_TinyPNG_Block_Adminhtml_System_Config_Form extends Mage_Adminhtml_Block_System_Config_Form
+{
+    /**
+     * Add a new element types.
+     *
+     * @return array
+     */
+    protected function _getAdditionalElementTypes()
+    {
+        $elementTypes = parent::_getAdditionalElementTypes();
+
+        $elementTypes['tinypng_radios'] = Mage::getConfig()
+            ->getBlockClassName('tig_tinypng/adminhtml_system_config_form_field_radios');
+
+        return $elementTypes;
+    }
+}

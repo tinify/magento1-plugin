f5773fbc5cd78af8911052a2d4402dd67d4222dd
Michiel Gerritsen <michiel.gerritsen@tig.nl>
2016-05-30 15:13:44 +0200

    Changed the namespace from TIG_TinyPNG to Tiny_CompressImages

---
+++ app/code/community/Tiny/CompressImages/Block/Adminhtml/System/Config/Form/Field/Saved.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ *                  ___________       __            __
+ *                  \__    ___/____ _/  |_ _____   |  |
+ *                    |    |  /  _ \\   __\\__  \  |  |
+ *                    |    | |  |_| ||  |   / __ \_|  |__
+ *                    |____|  \____/ |__|  (____  /|____/
+ *                                              \/
+ *          ___          __                                   __
+ *         |   |  ____ _/  |_   ____ _______   ____    ____ _/  |_
+ *         |   | /    \\   __\_/ __ \\_  __ \ /    \ _/ __ \\   __\
+ *         |   ||   |  \|  |  \  ___/ |  | \/|   |  \\  ___/ |  |
+ *         |___||___|  /|__|   \_____>|__|   |___|  / \_____>|__|
+ *                  \/                           \/
+ *                  ________
+ *                 /  _____/_______   ____   __ __ ______
+ *                /   \  ___\_  __ \ /  _ \ |  |  \\____ \
+ *                \    \_\  \|  | \/|  |_| ||  |  /|  |_| |
+ *                 \______  /|__|    \____/ |____/ |   __/
+ *                        \/                       |__|
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to servicedesk@totalinternetgroup.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact servicedesk@totalinternetgroup.nl for more information.
+ *
+ * @copyright   Copyright (c) 2016 Total Internet Group B.V. (http://www.totalinternetgroup.nl)
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+class Tiny_CompressImages_Block_Adminhtml_System_Config_Form_Field_Saved extends Varien_Data_Form_Element_Abstract
+{
+    /**
+     * Generate the status for the TinyPNG extension.
+     *
+     * @return string
+     */
+    public function getElementHtml()
+    {
+        $data = Mage::getModel('tig_tinypng/totals')->getTotalCompressionInformation();
+
+        return Mage::helper('tig_tinypng')->__(
+            'Saved %s%% over a total of %s compressions',
+            $data['percentageSaved'],
+            $data['totalCompressions']
+        );
+
+    }
+}
---
+++ app/code/community/Tiny/CompressImages/Test/Framework/TIG/Test/Config.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ *                  ___________       __            __
+ *                  \__    ___/____ _/  |_ _____   |  |
+ *                    |    |  /  _ \\   __\\__  \  |  |
+ *                    |    | |  |_| ||  |   / __ \_|  |__
+ *                    |____|  \____/ |__|  (____  /|____/
+ *                                              \/
+ *          ___          __                                   __
+ *         |   |  ____ _/  |_   ____ _______   ____    ____ _/  |_
+ *         |   | /    \\   __\_/ __ \\_  __ \ /    \ _/ __ \\   __\
+ *         |   ||   |  \|  |  \  ___/ |  | \/|   |  \\  ___/ |  |
+ *         |___||___|  /|__|   \_____>|__|   |___|  / \_____>|__|
+ *                  \/                           \/
+ *                  ________
+ *                 /  _____/_______   ____   __ __ ______
+ *                /   \  ___\_  __ \ /  _ \ |  |  \\____ \
+ *                \    \_\  \|  | \/|  |_| ||  |  /|  |_| |
+ *                 \______  /|__|    \____/ |____/ |   __/
+ *                        \/                       |__|
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to servicedesk@tig.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact servicedesk@tig.nl for more information.
+ *
+ * @copyright   Copyright (c) 2015 Total Internet Group B.V. (http://www.tig.nl)
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+class Tiny_CompressImages_Test_Framework_TIG_Test_Config extends Mage_Core_Model_Config
+{
+    /**
+     * @var array
+     */
+    protected $_mockModels = array();
+
+    /**
+     * @var array
+     */
+    protected $_mockResourceModels = array();
+
+    /**
+     * @param string $modelClass
+     * @param object $mock
+     *
+     * @return $this
+     */
+    public function setModelMock($modelClass, $mock)
+    {
+        $this->_mockModels[$modelClass] = $mock;
+        return $this;
+    }
+
+    /**
+     * @param string $modelClass
+     * @param object $mock
+     *
+     * @return $this
+     */
+    public function setResourceModelMock($modelClass, $mock)
+    {
+        $this->_mockResourceModels[$modelClass] = $mock;
+        return $this;
+    }
+
+    /**
+     * @param string $modelClass
+     * @param array  $constructArguments
+     *
+     * @return false|Mage_Core_Model_Abstract
+     */
+    public function getModelInstance($modelClass = '', $constructArguments = array())
+    {
+        $modelClass = (string) $modelClass;
+
+        if (array_key_exists($modelClass, $this->_mockModels)) {
+            return $this->_mockModels[$modelClass];
+        }
+
+        return parent::getModelInstance($modelClass, $constructArguments);
+    }
+
+    /**
+     * Get resource model object by alias
+     *
+     * @param   string $modelClass
+     * @param   array $constructArguments
+     * @return  object
+     */
+    public function getResourceModelInstance($modelClass='', $constructArguments = array())
+    {
+        $modelClass = (string) $modelClass;
+
+        if (array_key_exists($modelClass, $this->_mockResourceModels)) {
+            return $this->_mockResourceModels[$modelClass];
+        }
+
+        return parent::getResourceModelInstance($modelClass, $constructArguments);
+    }
+}

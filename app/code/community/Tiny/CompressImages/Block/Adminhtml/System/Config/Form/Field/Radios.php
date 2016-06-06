49ff66e055ac9647b126a8674f40c0f84ffa26ca
Michiel Gerritsen <michiel.gerritsen@tig.nl>
2016-06-06 15:54:05 +0200

    Removed the licences from the files

--- app/code/community/Tiny/CompressImages/Block/Adminhtml/System/Config/Form/Field/Radios.php
+++ app/code/community/Tiny/CompressImages/Block/Adminhtml/System/Config/Form/Field/Radios.php
@@ -19,13 +19,7 @@
  *                 \______  /|__|    \____/ |____/ |   __/
  *                        \/                       |__|
  *
- * NOTICE OF LICENSE
  *
- * This source file is subject to the Creative Commons License.
- * It is available through the world-wide-web at this URL:
- * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
- * If you are unable to obtain it through the world-wide-web, please send an email
- * to servicedesk@totalinternetgroup.nl so we can send you a copy immediately.
  *
  * DISCLAIMER
  *
@@ -34,7 +28,6 @@
  * needs please contact servicedesk@totalinternetgroup.nl for more information.
  *
  * @copyright   Copyright (c) 2016 Total Internet Group B.V. (http://www.totalinternetgroup.nl)
- * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
  */
 class Tiny_CompressImages_Block_Adminhtml_System_Config_Form_Field_Radios extends Varien_Data_Form_Element_Radios
 {

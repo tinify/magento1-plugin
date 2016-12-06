d8267693493ac35501409a23f2ac6dfe69bf38e8
Michiel Gerritsen <michiel.gerritsen@tig.nl>
2016-12-06 15:31:42 +0100

    - Fix: When open basedir was enable sometimes an exception would be throwed which prevented the compression. - Improvement: Catch any expection throw during compression and log this.

--- app/code/community/Tiny/CompressImages/Model/Observer.php
+++ app/code/community/Tiny/CompressImages/Model/Observer.php
@@ -7,6 +7,11 @@ class Tiny_CompressImages_Model_Observer
     protected $_tinifyHelper = null;
 
     /**
+     * @var null|Tiny_CompressImages_Helper_Data
+     */
+    protected $_dataHelper = null;
+
+    /**
      * Compress product images
      *
      * @param $observer
@@ -19,8 +24,18 @@ class Tiny_CompressImages_Model_Observer
             $this->_tinifyHelper = Mage::helper('tiny_compressimages/tinify');
         }
 
-        $storeId = Mage::app()->getStore()->getStoreId();
-        $this->_tinifyHelper->setProductImage($observer->getObject(), $storeId)->compress();
+        if ($this->_dataHelper === null) {
+            $this->_dataHelper = Mage::helper('tiny_compressimages');
+        }
+
+        try {
+            $storeId = Mage::app()->getStore()->getStoreId();
+            $this->_tinifyHelper->setProductImage($observer->getObject(), $storeId)->compress();
+        } catch (Exception $e) {
+            $this->_dataHelper->log($e);
+
+            throw $e;
+        }
 
         return $this;
     }

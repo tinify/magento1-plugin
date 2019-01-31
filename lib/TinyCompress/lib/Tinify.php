fed55cd08ad6df4f4fa3878d3cf833c601b4342a
Dennis van der Hammen <dennis.van.der.hammen@tig.nl>
2019-01-31 16:28:54 +0100

    Updated TinyPNG library

--- lib/TinyCompress/lib/Tinify.php
+++ lib/TinyCompress/lib/Tinify.php
@@ -2,12 +2,21 @@
 
 namespace Tinify;
 
-const VERSION = "1.3.0";
+const VERSION = "1.5.2";
 
 class Tinify {
+    const AUTHENTICATED = true;
+    const ANONYMOUS = false;
+
     private static $key = NULL;
     private static $appIdentifier = NULL;
+    private static $proxy = NULL;
+
     private static $compressionCount = NULL;
+    private static $remainingCredits = NULL;
+    private static $payingState = NULL;
+    private static $emailAddress = NULL;
+
     private static $client = NULL;
 
     public static function setKey($key) {
@@ -15,8 +24,23 @@ class Tinify {
         self::$client = NULL;
     }
 
+    public static function getKey() {
+        return self::$key;
+    }
+
+    public static function createKey($email, $options) {
+        $body = array_merge(array("email" => $email), $options);
+        $response = self::getClient(self::ANONYMOUS)->request("post", "/keys", $body);
+        self::setKey($response->body->key);
+    }
+
     public static function setAppIdentifier($appIdentifier) {
         self::$appIdentifier = $appIdentifier;
+        self::$client = NULL;
+    }
+
+    public static function setProxy($proxy) {
+        self::$proxy = $proxy;
         self::$client = NULL;
     }
 
@@ -28,13 +52,37 @@ class Tinify {
         self::$compressionCount = $compressionCount;
     }
 
-    public static function getClient() {
-        if (!self::$key) {
+    public static function getRemainingCredits() {
+        return self::$remainingCredits;
+    }
+
+    public static function setRemainingCredits($remainingCredits) {
+        self::$remainingCredits = $remainingCredits;
+    }
+
+    public static function getPayingState() {
+        return self::$payingState;
+    }
+
+    public static function setPayingState($payingState) {
+        self::$payingState = $payingState;
+    }
+
+    public static function getEmailAddress() {
+        return self::$emailAddress;
+    }
+
+    public static function setEmailAddress($emailAddress) {
+        self::$emailAddress = $emailAddress;
+    }
+
+    public static function getClient($mode = self::AUTHENTICATED) {
+        if ($mode == self::AUTHENTICATED && !self::$key) {
             throw new AccountException("Provide an API key with Tinify\setKey(...)");
         }
 
         if (!self::$client) {
-            self::$client = new Client(self::$key, self::$appIdentifier);
+            self::$client = new Client(self::$key, self::$appIdentifier, self::$proxy);
         }
 
         return self::$client;
@@ -49,8 +97,20 @@ function setKey($key) {
     return Tinify::setKey($key);
 }
 
+function getKey() {
+    return Tinify::getKey();
+}
+
+function createKey($email, $options) {
+    return Tinify::createKey($email, $options);
+}
+
 function setAppIdentifier($appIdentifier) {
     return Tinify::setAppIdentifier($appIdentifier);
+}
+
+function setProxy($proxy) {
+    return Tinify::setProxy($proxy);
 }
 
 function getCompressionCount() {
@@ -59,6 +119,18 @@ function getCompressionCount() {
 
 function compressionCount() {
     return Tinify::getCompressionCount();
+}
+
+function remainingCredits() {
+    return Tinify::getRemainingCredits();
+}
+
+function payingState() {
+    return Tinify::getPayingState();
+}
+
+function emailAddress() {
+    return Tinify::getEmailAddress();
 }
 
 function fromFile($path) {
@@ -75,7 +147,8 @@ function fromUrl($string) {
 
 function validate() {
     try {
-        Tinify::getClient()->request("post", "/shrink");
+        Tinify::getClient()->request("get", "/keys/" . Tinify::getKey());
+        return true;
     } catch (AccountException $err) {
         if ($err->status == 429) return true;
         throw $err;
